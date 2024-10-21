1bf276da1d16fb1680bfb2a5125b2bc410f5c8c4
E. Barwin Raj <barwin15102001@gmail.com>
2024-10-21 18:21:41 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base:"/Portfolio_Website_in_Vite_and_React/",
+  server: {
+    historyApiFallback: true, // Ensures all routes point to index.html
+  },
   plugins: [react()],
 })
