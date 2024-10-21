697b3b25e49e0645594ff72af1c5c8c0730c01bb
E. Barwin Raj <barwin15102001@gmail.com>
2024-10-21 19:03:37 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Portfolio_Website_in_Vite_and_React/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Portfolio_Website_in_Vite_and_React/",
-  server: {
-    historyApiFallback: true, // Ensures all routes point to index.html
-  },
   plugins: [react()],
 })
