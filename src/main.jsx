66a219aa22ea357540d897649143968d7bcaf049
E. Barwin Raj <barwin15102001@gmail.com>
2024-10-21 18:04:28 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Portfolio_Website_in_Vite_and_React/">
       <App />
     </BrowserRouter>
   </StrictMode>
