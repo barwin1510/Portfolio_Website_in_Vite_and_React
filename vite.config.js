d55cef6f35f478cff558468a8a68a379cfe26597
E. Barwin Raj <barwin15102001@gmail.com>
2024-10-23 19:19:34 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/Portfolio_Website_in_Vite_and_React/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: null
+      }
+    }
+  }
 });
