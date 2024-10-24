4338757ebabd841c79b97171dcb23990e615b075
E. Barwin Raj <barwin15102001@gmail.com>
2024-10-24 10:02:16 +0530

    updated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -22,22 +22,22 @@ const Navbar=()=>{
                 <h3>Barwin Raj</h3>
             </nav>
             <nav className="rnav">
-                    <p className="navsub1"><a href='/' className="nsr1">HOME</a></p>
-                    <p className="navsub2"><a href='/About' className="nsr2">ABOUT</a></p>
-                    <p className="navsub3"><a href='/Skill' className="nsr3">SKILL</a></p>
-                    <p className="navsub4"><a href='/Project' className="nsr4">PROJECT</a></p>
-                    <p className="navsub5"><a href='/Contact' className="nsr5">CONTACT</a></p>
+                    <p className="navsub1"><Link to='/' className="nsr1">HOME</Link></p>
+                    <p className="navsub2"><Link to='/About' className="nsr2">ABOUT</Link></p>
+                    <p className="navsub3"><Link to='/Skill' className="nsr3">SKILL</Link></p>
+                    <p className="navsub4"><Link to='/Project' className="nsr4">PROJECT</Link></p>
+                    <p className="navsub5"><Link to='/Contact' className="nsr5">CONTACT</Link></p>
             </nav>
             <p className="menuP"><img onClick={sidenavOpen} className="menuIcon" src={Menu}></img></p>
         </nav>
             <nav className="sidenav" style={{ left: sidenavVisible ? '0%' : '-50%'}}>
                 <p className="cancelP"><img className="cancelIcon" onClick={sidenavClose} src={Cancel}></img></p>
                 <nav className="navside">
-                    <p className="navside1"><a href='/' className="lnsr1">HOME</a></p><br></br>
-                    <p className="navside2"><a href='/About' className="lnsr2">ABOUT</a></p><br></br>
-                    <p className="navside3"><a href='/Skill' className="lnsr3">SKILL</a></p><br></br>
-                    <p className="navside4"><a href='/Project' className="lnsr4">PROJECT</a></p><br></br>
-                    <p className="navside5"><a href='/Contact' className="lnsr5">CONTACT</a></p>
+                    <p className="navside1"><Link to='/' className="lnsr1">HOME</Link></p><br></br>
+                    <p className="navside2"><Link to='/About' className="lnsr2">ABOUT</Link></p><br></br>
+                    <p className="navside3"><Link to='/Skill' className="lnsr3">SKILL</Link></p><br></br>
+                    <p className="navside4"><Link to='/Project' className="lnsr4">PROJECT</Link></p><br></br>
+                    <p className="navside5"><Link to='/Contact' className="lnsr5">CONTACT</Link></p>
                 </nav>
             </nav>
         </>
